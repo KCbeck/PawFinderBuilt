1f0280e6edebc9b60db8cb17c808195715c781cd
KCbeck <50602600+KCbeck@users.noreply.github.com>
2019-09-20 20:59:29 -0500

    newest update

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>PetRoolette</h1>
+          <p>A Pet Random Adoption Page</p>
+        </div>
+      </div>
+    );
+  }
+}
